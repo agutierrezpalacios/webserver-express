761cafe661b2d81ce472f00c6d48ab9e04e555a4
Angel <refresquito111@gmail.com>
2019-12-24 17:59:22 +0100

    init

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers')
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Angel',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {
+        nombre: 'Angel',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/data', (req, res) => {
+    res.send('Hola data');
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+})
